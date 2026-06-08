26ff94df57c71aae9161f0a1abdff721abe78599
Brad Blanchette <bradblanchette@outlook.com>
2026-06-08 21:06:09 +0100

    Add Footer component with copyright year display

---
+++ job-listing-app/src/components/Footer.vue
@@ -0,0 +1,17 @@
+<script>
+export default {
+    computed: {
+        getYear() {
+            return new Date().getFullYear();
+        }
+    }
+}
+</script>
+
+<template>
+    <footer class="bg-blue-900 p-4 text-white">
+        <div class="text-center">
+            <span>Copyright &copy; {{ getYear }}</span>
+        </div>
+    </footer>
+</template>
