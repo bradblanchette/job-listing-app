890e8bcac253267244aec6a54cc3722c0e30a504
Brad Blanchette <bradblanchette@outlook.com>
2026-06-25 20:43:33 +0100

    Add loading spinner to List component during data fetch

--- job-listing-app/src/views/List.vue
+++ job-listing-app/src/views/List.vue
@@ -2,11 +2,13 @@
   export default {
     data() {
       return {
+        loading: false,
         jobs: [],
         error: null
       }
     },
     async mounted() {
+      this.loading = true;
       try {
         const res = await fetch('https://fakejobs-api.vercel.app/jobs');
         if (!res.ok) {
@@ -17,6 +19,9 @@
       catch(err) {
         this.error = err.message;
       }
+      finally {
+        this.loading = false;
+      }
     }
   }
 </script>
@@ -24,5 +29,6 @@
 <template>
   <div class="max-w-7xl m-auto p-2">
     <h1>List Page</h1>
+    <div v-if="loading" class="w-10 h-10 rounded-full border-4 border-gray-200 border-t-blue-900 animate-spin m-auto"></div>
   </div>
 </template>
